a0db78e9a6b51c54989424686edd5d2d4c41879b
Mai Khang <mhkhang.khmt0117@student.ctuet.edu.vn>
2021-01-13 14:19:45 +0700

    add tourController, userController

---
+++ src/app/controllers/userController.js
@@ -0,0 +1,35 @@
+
+exports.getAllUser = (req, res) => {
+    res.status(500).json({
+        status: 'err',
+        message: 'This Route is not defined'
+    });
+}
+
+exports.getUser = (req, res) => {
+    res.status(500).json({
+        status: 'err',
+        message: 'This Route is not defined'
+    });
+}
+
+exports.createNewUser = (req, res) => {
+    res.status(500).json({
+        status: 'err',
+        message: 'This Route is not defined'
+    });
+}
+
+exports.updateUser = (req, res) => {
+    res.status(500).json({
+        status: 'err',
+        message: 'This Route is not defined'
+    });
+}
+
+exports.deleteUser = (req, res) => {
+    res.status(500).json({
+        status: 'err',
+        message: 'This Route is not defined'
+    });
+}
