5348c6c1067f54025c4e453be9ad29002810b26b
Mai Khang <mhkhang.khmt0117@student.ctuet.edu.vn>
2021-01-17 15:50:29 +0700

    update Routes(tourRoutes, userRoutes)

--- src/routes/tourRoutes.js
+++ src/routes/tourRoutes.js
@@ -1,16 +1,17 @@
 const express = require('express');
-const tourController = require('../app/controllers/tourController');
+const tourController = require('../app/controllers/TourController');
 
 // Tours
 const router = express.Router();
-router.param('id', tourController.checkID);
 
-router.route('/')
-    .get(tourController.getAllTour)
-    .post(tourController.postMiddleware, tourController.createNewTour);
-router.route('/:id')
-    .get(tourController.getTour)
-    .patch(tourController.updateTour)
-    .delete(tourController.deleteTour);
+router
+	.route('/')
+	.get(tourController.getAllTour)
+	.post(tourController.createTour);
+router
+	.route('/:id')
+	.get(tourController.getTour)
+	.patch(tourController.updateTour)
+	.delete(tourController.deleteTour);
 
 module.exports = router;
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,16 +1,18 @@
 const express = require('express');
-const userController = require('../app/controllers/userController');
+const userController = require('../app/controllers/UserController');
 
 // Users
 const router = express.Router();
 
-router.route('/')
-    .get(userController.getAllUser)
-    .post(userController.createNewUser);
+router
+	.route('/')
+	.get(userController.getAllUser)
+	.post(userController.createNewUser);
 
-router.route('/:id')
-    .get(userController.getUser)
-    .patch(userController.updateUser)
-    .delete(userController.deleteUser);
+router
+	.route('/:id')
+	.get(userController.getUser)
+	.patch(userController.updateUser)
+	.delete(userController.deleteUser);
 
 module.exports = router;
